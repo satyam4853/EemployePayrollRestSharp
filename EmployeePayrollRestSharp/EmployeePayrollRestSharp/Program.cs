8d26a77f2da5f035949e26e82ce8eafaa3c213ba
satyam4853 <vaishnavsatyam48532gmail.com>
2021-09-15 00:48:32 +0530

    [satyam] Welcome Message Added

---
+++ EmployeePayrollRestSharp/EmployeePayrollRestSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollRestSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------Employee Payroll Using RestSharp-----------------");
+        }
+    }
+}
