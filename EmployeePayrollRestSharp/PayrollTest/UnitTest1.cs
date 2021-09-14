65b54b499559325f76b5576f7758c996179b4071
satyam4853 <vaishnavsatyam48532gmail.com>
2021-09-15 01:24:00 +0530

    [satyam] feat:Add Multiple Data To Server File

--- EmployeePayrollRestSharp/PayrollTest/UnitTest1.cs
+++ EmployeePayrollRestSharp/PayrollTest/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace PayrollTest
     [TestClass]
     public class PayrollRestSharpTest
     {
-        //Initializing the restclient 
+        //Initializing the restclient
         RestClient client;
 
         [TestInitialize]
@@ -26,10 +26,19 @@ namespace PayrollTest
         /// <returns></returns>
         public IRestResponse GetAllEmployee()
         {
-            //Get request from json server
-            RestRequest request = new RestRequest("/employees", Method.GET);
-            //Requesting server and execute , getting response
-            IRestResponse response = client.Execute(request);
+            IRestResponse response = default;
+            try
+            {
+                //Get request from json server
+                RestRequest request = new RestRequest("/employees", Method.GET);
+                //Requesting server and execute , getting response
+                response = client.Execute(request);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
 
             return response;
         }
@@ -39,19 +48,27 @@ namespace PayrollTest
         [TestMethod]
         public void TestMethodToGetAllEmployees()
         {
-            //calling get all employee method 
-            IRestResponse response = GetAllEmployee();
-            //converting response to list og objects
-            var res = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            //Check whether all contents are received or not
-            Assert.AreEqual(3, res.Count);
-            //Checking the response statuscode 200-ok
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
-
-            res.ForEach((x) =>
+            try
             {
-                Console.WriteLine($"id = {x.id} , name = {x.name} , salary = {x.salary}");
-            });
+                //calling get all employee method 
+                IRestResponse response = GetAllEmployee();
+                //converting response to list og objects
+                var res = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+                //Check whether all contents are received or not
+                Assert.AreEqual(3, res.Count);
+                //Checking the response statuscode 200-ok
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+                res.ForEach((x) =>
+                {
+                    Console.WriteLine($"id = {x.id} , name = {x.name} , salary = {x.salary}");
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
 
 
         }
@@ -62,12 +79,20 @@ namespace PayrollTest
         /// <returns></returns>
         public IRestResponse AddToJsonServer(JsonObject json)
         {
-            RestRequest request = new RestRequest("/employees", Method.POST);
-            //adding type as json in request and pasing the json object as a body of request
-            request.AddParameter("application/json", json, ParameterType.RequestBody);
+            IRestResponse response = default;
+            try
+            {
+                RestRequest request = new RestRequest("/employees", Method.POST);
+                //adding type as json in request and pasing the json object as a body of request
+                request.AddParameter("application/json", json, ParameterType.RequestBody);
 
-            //Execute the request
-            IRestResponse response = client.Execute(request);
+                //Execute the request
+                response = client.Execute(request);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
             return response;
 
         }
@@ -77,27 +102,75 @@ namespace PayrollTest
         [TestMethod]
         public void TestMethodToAddEmployeeToJsonServerFile()
         {
-            //Setting rest rquest to url and setiing method to post
-            RestRequest request = new RestRequest("/employees", Method.POST);
-            //object for json
-            JsonObject json = new JsonObject();
-            //Adding new employee details to json object
-            json.Add("name", "Neymar");
-            json.Add("salary", 13000);
+            try
+            {
+                //Setting rest rquest to url and setiing method to post
+                RestRequest request = new RestRequest("/employees", Method.POST);
+                //object for json
+                JsonObject json = new JsonObject();
+                //Adding new employee details to json object
+                json.Add("name", "Raju");
+                json.Add("salary", 13000);
 
-            //calling method to add to server
-            IRestResponse response = AddToJsonServer(json);
-            //deserialize json objject to employee class  object
-            var res = JsonConvert.DeserializeObject<Employee>(response.Content);
+                //calling method to add to server
+                IRestResponse response = AddToJsonServer(json);
+                //deserialize json objject to employee class  object
+                var res = JsonConvert.DeserializeObject<Employee>(response.Content);
 
-            //Checking the response statuscode 201-created
-            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+                //Checking the response statuscode 201-created
+                Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
 
-            //checking object values
-            Assert.AreEqual("Messi", res.name);
-            Console.WriteLine($"id = {res.id} , name = {res.name} , salary = {res.salary}");
+                //checking object values
+                Assert.AreEqual("Omi", res.name);
+                Console.WriteLine($"id = {res.id} , name = {res.name} , salary = {res.salary}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+        /// <summary>
+        /// test Method to add multiple data to the json server
+        /// </summary>
+        [TestMethod]
+        public void TestForAddMultipleDataToJsonServerFile()
+        {
+            try
+            {
+                //list for storing multiple employeee data json objects
+                List<JsonObject> employeeList = new List<JsonObject>();
+                JsonObject json = new JsonObject();
+                json.Add("name", "Shankar");
+                json.Add("salary", 40000);
+                //add object to list
+                employeeList.Add(json);
+                JsonObject json1 = new JsonObject();
+                json1.Add("name", "Alex");
+                json1.Add("salary", 35000);
+                employeeList.Add(json1);
 
+                employeeList.ForEach((x) =>
+                {
+                    AddToJsonServer(x);
+                });
+                //Check by gettting all employee details
+                IRestResponse response = GetAllEmployee();
+                //convert json object to employee object
+                var res = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+
+                res.ForEach((x) =>
+                {
+                    Console.WriteLine($"id = {x.id} , name = {x.name} , salary = {x.salary}");
+                });
+                //Checking the response statuscode 200-ok
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
 
     }
 }
